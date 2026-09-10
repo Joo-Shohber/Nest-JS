4bd20ef3615ad44e020d00984774ba4c86376062
Sonic-Joo <yousefshohber0123@gmail.com>
2026-09-10 16:55:53 +0300

    fix vercel deployment

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import helmet from 'helmet';
+import { config } from 'dotenv';
+
+config({ path: '.env.development' });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
