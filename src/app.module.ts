1db09b2ba020e4878a32a2906e35c35319ce75a3
Sonic-Joo <yousefshohber0123@gmail.com>
2026-09-16 18:29:33 +0300

    redis setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,7 +9,9 @@ import { MailModule } from './mail/mail.module';
 import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
 import { dataSourceOptions } from '../db/data-source';
 import { AppController } from './app.controller';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { CacheModule } from '@nestjs/cache-manager';
+import { redisStore } from 'cache-manager-redis-yet';
 
 @Module({
   controllers: [AppController],
@@ -33,6 +35,22 @@ import { ConfigModule } from '@nestjs/config';
         limit: 10,
       },
     ]),
+    CacheModule.registerAsync({
+      isGlobal: true,
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => {
+        return {
+          store: await redisStore({
+            socket: {
+              host: config.get<string>('REDIS_HOST'),
+              port: config.get<number>('REDIS_PORT'),
+            },
+            password: config.get<string>('REDIS_PASSWORD'),
+            ttl: 60_000,
+          }),
+        };
+      },
+    }),
   ],
 
   providers: [
